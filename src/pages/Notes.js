8aab95b953cac89731a05def94f8dd5bd39e5902
AnsonCHOWcm <74531677+AnsonCHOWcm@users.noreply.github.com>
2023-01-27 16:11:38 +0800

    finished bulti-in modules : os path fs

--- src/pages/Notes.js
+++ src/pages/Notes.js
@@ -4,8 +4,7 @@ import Node_Global from "../img/Node_Global.jpg";
 const Notes = () => {
     return (
         <div style = {{padding : "10px", marginTop : "0"}}>
-            <h1>Notes</h1>
-            <h2>Node Js</h2>
+            <h1>Node Js</h1>
             <p>It is an environment to run JS outside Browser
                 <br></br>
                 Therefore, there is no DOM, Window 
@@ -28,6 +27,32 @@ const Notes = () => {
             </p>
             <h4>Import</h4>
             <p>To import the information to the local files, we use syntax : "const VAL = required(MODULE_PATH)"</p>
+            <h2>Bulti-in Module : OS</h2>
+            <p>Syntax : const os = require("os")
+                <br></br>
+                This is a module allowing developers to communicate with the Operating System
+            </p>
+            <h2>Bulti-in Module : path</h2>
+            <p>Syntax : const path = require("path")
+                <br></br>
+                This is a module allowing developers to interact with the file locations/paths
+            </p>
+            <h2>Bulti-in Module : FileSystem</h2>
+            <p>Syntax : const fs = require('fs')
+                <br></br>
+                This is module allowing developers to interact with file system
+                <br></br>
+                There are two main functions : readFile and writeFile
+                <br></br>
+                For each function, there are two versions : sync and async
+                <br></br>
+                readFile Syntax : "readFile" (async) or "readFileSync" (sync)
+                <br></br>
+                writeFile Syntax : "writeFile" (async) or "writeFileSync" (sync)
+                <br></br>
+                For the async function : developers can pass callback function after the original function executed
+            </p>
+            <p></p>
         </div>
 
     );
